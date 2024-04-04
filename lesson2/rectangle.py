16b2df24c2611595d656138fb13321fe2ff604b4
Evgeniy Erokhov <evgeniy.erokhov@gmail.com>
2024-04-05 03:02:38 +0500

    person.py

--- lesson2/rectangle.py
+++ lesson2/rectangle.py
@@ -4,14 +4,36 @@
 - __init__(self, width, height): конструктор, принимающий ширину и высоту прямоугольника
 - area(self): метод, возвращающий площадь прямоугольника
 - perimeter(self): метод, возвращающий периметр прямоугольника
-- from_diagonal(cls, diagonal, aspect_ratio): класс-метод, принимающий диагональ прямоугольника и соотношение сторон и возвращающий объект класса Rectangle
+- from_diagonal(cls, diagonal, aspect_ratio): класс-метод, принимающий диагональ прямоугольника
+ и соотношение сторон и возвращающий объект класса Rectangle
 - is_square(width, height): статический метод, принимающий ширину и высоту прямоугольника и возвращающий True,
 если это квадрат, и False в противном случае
 """
 
 
 class Rectangle:
-    pass
+
+    def __init__(self, widht: int, height: int):
+        self.widht = widht
+        self.height = height
+
+    def area(self) -> float:
+        area = self.widht * self.height
+        return area
+
+    def perimeter(self):
+        perimeter = (self.widht + self.height) * 2
+        return perimeter
+
+    @classmethod
+    def from_diagonal(cls, diagonal, aspect_ratio):
+        wight = (diagonal ** 2 / (1 + aspect_ratio ** 2)) ** 0.5
+        height = aspect_ratio * wight
+        return cls(wight, height)
+
+    @staticmethod
+    def is_square(wight, height):
+        return wight == height
 
 
 # код для проверки 
