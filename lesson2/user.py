70f7eb7e35ff19d6bcbf04e16fc095e5dc08f5ca
Evgeniy Erokhov <evgeniy.erokhov@gmail.com>
2024-04-06 03:55:09 +0500

    user.py

--- lesson2/user.py
+++ lesson2/user.py
@@ -2,19 +2,50 @@
 Напишите класс User, имеющий следующие свойства и методы:
 
 - __init__(self, name, password): конструктор, принимающий имя пользователя и пароль
-- name: свойство, которое возвращает имя пользователя
-- password: свойство, которое позволяет установить или изменить пароль пользователя
-- is_admin: свойство, которое возвращает, является ли пользователь администратором или нет
-- _is_admin: свойство-помощник, которое определяет, является ли пользователь администратором или нет
-- login(self, password): метод, который проверяет, соответствует ли введенный пароль паролю пользователя
-- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False при условии, что пользователь залогинен)
+- name: свойство, которое возвращает имя пользователя.
+- password: свойство, которое позволяет установить или изменить пароль пользователя.
+- is_admin: свойство, которое возвращает, является ли пользователь администратором или нет.
+- _is_admin: свойство-помощник, которое определяет, является ли пользователь администратором или нет.
+- login(self, password): метод, который проверяет, соответствует ли введенный пароль паролю пользователя.
+- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False
+при условии, что пользователь залогинен)
 
 Для свойств name и password используйте декораторы @property и @password.setter.
 """
 
 
 class User:
-    pass
+
+    def __init__(self, name: str, password: str):
+        self.__name = name
+        self.__password = password
+        self._is_admin = False
+        self._is_logged_in = False
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def password(self) -> str:
+        return self.__password
+
+    @password.setter
+    def password(self, value) -> None:
+        self.__password = value
+
+    @property
+    def is_admin(self) -> bool:
+        return self._is_admin
+
+    def login(self, password):
+        self._is_logged_in = self.password == password
+        return self._is_logged_in
+
+    def logout(self) -> None:
+        if not self._is_logged_in:
+            raise Exception('User was not logged in.')
+        self._is_logged_in = False
 
 
 # код для проверки 
@@ -29,5 +60,21 @@ print(user1.password)  # newpassword
 user1._is_admin = True
 print(user1.is_admin)  # True
 
-user1.login("newpassword")  # True
-user1.logout()
+if user1.login("newpassword"):  # True
+    print('User was logged in.')
+    user1.logout()
+
+users = [
+    User('asd', 'qe'),
+    User('eweq', '213'),
+    User('qe', '123')
+]
+
+user_name_1 = 'asd'
+user_name_2 = 'asd'
+user_name_3 = 'asd'
+
+user_names = [user_name_3, user_name_2, user_name_1]
+
+for user in users:
+    print('User ' + user.name)
